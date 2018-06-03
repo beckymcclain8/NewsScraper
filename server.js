b7b8d74f927e9a990b25fa429aca958cb2f0331a
Becky McClain <beckykelley8@yahoo.com>
2018-06-02 17:04:49 -0700

    setup

---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+//ROUTES
+app.get("/", function(req, res) {
+    // First, we grab the body of the html with request
+    axios.get("http://www.echojs.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article h2").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            var hbsObject = {
+                articles: dbArticle
+              };
+              console.log("hbsObject", hbsObject);
+            res.render("index", hbsObject);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+      });
+  
+      // If we were able to successfully scrape and save an Article, send a message to the client
+    //   res.send("Scrape Complete");
+    });
+  });
+
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
