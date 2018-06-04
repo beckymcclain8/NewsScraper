035e36debf5c1ef2066106b64ea7939348729ca7
Becky McClain <beckykelley8@yahoo.com>
2018-06-04 13:20:10 -0700

    moved route to app.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
 
 var db = require("./models");
 
@@ -26,51 +27,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-//ROUTES
-app.get("/", function(req, res) {
-    // First, we grab the body of the html with request
-    axios.get("http://www.echojs.com/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            var hbsObject = {
-                articles: dbArticle
-              };
-              console.log("hbsObject", hbsObject);
-            res.render("index", hbsObject);
-          })
-          .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
-      });
-  
-      // If we were able to successfully scrape and save an Article, send a message to the client
-    //   res.send("Scrape Complete");
-    });
-  });
+var routes = require("./public/app.js");
 
-
-
-
-
+app.use(routes);
 
 
 // Start the server
