786eed346387117029d5d4b38b6e3444797ed9a6
Becky McClain <beckykelley8@yahoo.com>
2018-06-09 15:44:02 -0500

    updates

--- public/app.js
+++ public/app.js
@@ -1,38 +1,65 @@
 
-  
-  // Whenever someone clicks a div tag
-  $(document).on("click", "div", function() {
-    // Empty the comments from the comment section
-    $("#comments").empty();
-    // Save the id from the div tag
+  $("#submit-info").on("click", function(event) {
+    // Make sure to preventDefault on a submit event.
+    console.log("button clicked");
+    event.preventDefault();
+
     var thisId = $(this).attr("data-id");
-   
-    // Now make an ajax call for the Article
+
+    // var newComment = {
+    //   body: $("#com").val().trim(),
+    //   };
+
+    // Send the POST request.
     $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#comments").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
-      });
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        body: $("#com").val().trim()
+      }
+    }).then(
+      function(data) {
+        console.log("data", data);
+        // Reload the page to get the updated list
+        // $("#comments").append(newComment);
+        location.reload();
+      }
+    );
   });
+
+  // // Whenever someone clicks a div tag
+  // $(document).on("click", "div", function() { 
+  //   // Empty the comments from the comment section
+  //   $("#comments").empty();
+  //   // Save the id from the div tag
+  //   var thisId = $(this).attr("data-id");
+   
+  //   // Now make an ajax call for the Article
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/articles/" + thisId
+  //   })
+  //     // With that done, add the note information to the page
+  //     .then(function(data) {
+  //       console.log(data);
+  //       // The title of the article
+  //       $("#comments").append("<h2>" + data.title + "</h2>");
+  //       // An input to enter a new title
+  //       $("#comments").append("<input id='titleinput' name='title' >");
+  //       // A textarea to add a new note body
+  //       $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
+  //       // A button to submit a new note, with the id of the article saved to it
+  //       $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+  
+  //       // If there's a note in the article
+  //       if (data.note) {
+  //         // Place the title of the note in the title input
+  //         $("#titleinput").val(data.note.title);
+  //         // Place the body of the note in the body textarea
+  //         $("#bodyinput").val(data.note.body);
+  //       }
+  //     });
+  // });
   
   // When you click the savenote button
   $(document).on("click", "#savenote", function() {
